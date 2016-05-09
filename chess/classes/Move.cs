ca110b8ffb7c20c1face445dfdd1026a2b62b2c6
MohabMohamed <mohab.m.mohamed@gmail.com>
2016-05-09 18:11:28 +0200

    Movement added with strategy pattern

---
+++ chess/classes/Move.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace chess
+{
+    class Move
+    {
+       private MoveBehavior movebehavior;
+       public Move(MoveBehavior movebehavior)
+       {
+           this.movebehavior = movebehavior;
+       } 
+       bool makemove(int oldrow, int oldcolmn, int newrow, int newcolmn)
+       {
+
+           return movebehavior.makemove(oldrow,oldcolmn,newrow,newcolmn);
+       }
+    }
+}
---
+++ chess/classes/Movement/ShortDiagonal.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace chess
+{
+    class ShortDiagonal:MoveBehavior
+    {
+       public bool makemove(int oldrow, int oldcolmn, int newrow, int newcolmn)
+        {
+            return true;
+        }
+    }
+}
